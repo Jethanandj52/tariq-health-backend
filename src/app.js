d8cefcf9a25ebba44cb92303f4718c46e2d3576c
Jethanand <134337163+Jethanandj52@users.noreply.github.com>
2025-10-30 01:33:04 +0500

    first commit

--- src/app.js
+++ src/app.js
@@ -15,9 +15,13 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tariq-health-frontend.vercel.app"],
+    origin: [
+      "https://tariq-health-frontend.vercel.app", // your frontend deployed domain
+      "http://localhost:5173" // for local testing
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
